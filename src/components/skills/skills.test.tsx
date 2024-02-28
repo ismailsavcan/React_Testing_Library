8d1ca1138c1e4fcb12ce14c36d847f87b0b99d70
ismail savcan <ismailsavcan@gmail.com>
2024-02-28 15:30:41 +0300

    findBy/methods added

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -1,31 +1,40 @@
-import {render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Skills } from './skills';
 
 describe('Skills', () => {
-    const skills =['HTML', 'CSS', 'Javascript', 'Typescript'];
+    const skills = ['HTML', 'CSS', 'Javascript', 'Typescript'];
 
     test('renders correctly', () => {
-        render(<Skills skills={skills}/>)
+        render(<Skills skills={skills} />)
         const listElement = screen.getByRole('list');
         expect(listElement).toBeInTheDocument();
     });
 
     test('renders list of skills', () => {
-        render(<Skills skills={skills}/>)
-        const listItemElements=screen.getAllByRole('listitem');
+        render(<Skills skills={skills} />)
+        const listItemElements = screen.getAllByRole('listitem');
         //expect(listItemElements).toHaveLength(3); // ->hard coding
         expect(listItemElements).toHaveLength(skills.length);
 
     });
     test('renders Login Button', () => {
-        render(<Skills skills={skills}/>)
-        const loginButton=screen.getByRole('button',{name:'Login'});
+        render(<Skills skills={skills} />)
+        const loginButton = screen.getByRole('button', { name: 'Login' });
         expect(loginButton).toBeInTheDocument();
     });
 
     test('Start Learning Button is not rendered', () => {
-        render(<Skills skills={skills}/>)
-        const startLearningButton=screen.queryByRole('button', {name:'Start learning'})
+        render(<Skills skills={skills} />)
+        const startLearningButton = screen.queryByRole('button', { name: 'Start learning' })
         expect(startLearningButton).not.toBeInTheDocument()
+    });
+
+    test('Start learning button is eventually displayed ', async () => {
+        render(<Skills skills={skills} />)
+        const startLearningButton = await screen.findByRole('button',
+            { name: 'Start learning' },
+            { timeout: 2000 }
+        );
+        expect(startLearningButton).toBeInTheDocument()
     });
 });
--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SkillsProps } from "./skills.types";
 
 export const Skills = (props: SkillsProps) => {
     const { skills } = props;
     const [isLoggedIn, setIsLoggedIn]=useState(false);
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            setIsLoggedIn(true)
+        }, 500);
+    }, []);
 
     return (
         <>
